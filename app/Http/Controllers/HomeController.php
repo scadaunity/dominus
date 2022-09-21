3aacf74131e138dd389ab0b09a8a4cab9f8e7059
scadaunity <scadaunity@gmail.com>
2022-09-21 06:40:03 -0300

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aluno;
+use App\Models\Professor;
 
 class HomeController extends Controller
 {
+
     public function index(){
-        return view('home');
+        $totalAlunos = Aluno::count();
+        $totalProfessores = Professor::count();
+        return view('home',[
+            'totalAlunos' => $totalAlunos,
+            'totalProfessores' => $totalProfessores
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,42 @@
 <x-layout title="Home">
+    <div class="row">
+        <!-- Total de alunos-->
+        <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-body">
+                  <div class="media d-flex">
+                    <div class="align-self-center">
+                      <i class="icon-pencil primary font-large-2 float-left"></i>
+                    </div>
+                    <div class="media-body text-right">
+                      <h3>{{$totalAlunos}}</h3>
+                      <span>Alunos</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+
+        <!-- Total de professores-->
+        <div class="col-xl-3 col-sm-6 col-12">
+            <div class="card">
+              <div class="card-content">
+                <div class="card-body">
+                  <div class="media d-flex">
+                    <div class="align-self-center">
+                      <i class="icon-pencil primary font-large-2 float-left"></i>
+                    </div>
+                    <div class="media-body text-right">
+                      <h3>{{$totalProfessores}}</h3>
+                      <span>Professores</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+  </div>
 
 </x-layout>
